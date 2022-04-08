531cbfdb0f8d47058e1c85929f3f053100f78904
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-08 15:11:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Display(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input the Limit of natural number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Display(n);
+
+
+        }
+    }
+}
